43ab08324cf9ad63732c32659ca7be81fd1fbaa2
Elias-87 <elias.assefa@gmail.com>
2025-11-03 17:54:04 -0700

    Assignment reuirements met

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,11 +4,14 @@
 async function main() {
   // This is where the code you're actually experimenting with goes.
 
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
+  let fname = await input("Please enter your first name: ");
+  let lname = await input("Please enter your last name: ");
+  let fullName = fname + " " + lname;
+  noOs = fullName.replaceAll("o", "a");
+  output(noOs);
 
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
-  }
+  let largeNum = await input("Please enter a number larger than 10: ");
+  let smolNum = await input("Please enter a number smaller than 10: ");
+  output(largeNum % smolNum);
 }
+
